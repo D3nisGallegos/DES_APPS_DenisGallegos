1edef33d23cb48203092fb9d1b07b44d89c73b6c
Denis <denisj.gallegos@unitec.edu>
2024-05-07 19:08:37 -0600

    2

--- api_connection.js
+++ api_connection.js
@@ -1,12 +1,8 @@
 const axios = require('axios');
 
-// Hacer una solicitud GET a la API Random User Generator
-axios.get('https://randomuser.me/api/')
-  .then(response => {
-    // Manejar la respuesta de la API
-    console.log(response.data); // Imprimir los datos en la consola
-  })
-  .catch(error => {
-    // Manejar errores
-    console.error('Error al hacer la solicitud:', error);
-  });
+// Función para hacer la solicitud a la API Random User Generator
+function fetchData() {
+  return axios.get('https://randomuser.me/api/');
+}
+
+module.exports = fetchData; // Exporta la función para que esté disponible para otros archivos
